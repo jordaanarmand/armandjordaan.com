308bf9445443b6ea7b5c5814d6f6ed22470888c2
Armand Jordaan <armandjordaan@live.co.za>
2022-10-13 13:39:28 +0200

    Add google analytics data

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-C3B335XJEL`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C3B335XJEL', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
